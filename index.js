9a0ff104a8aff54e7f69952b4326a129f9170347
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-11-25 23:02:18 +0530

    combine reducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 // Action
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_Icecream = 'BUY_Icecream'
@@ -18,30 +19,56 @@ function buyIcecream() {
 
 // reducer
 
-const intial = {
-    numCake : 10,
+const intialnumCake = {
+    numCake:10
+}
+const intialnumIcecream = {
     numIcecream:20
 }
 
 // copy of state object through ...state
 
-const reducer = (state=intial,action)=> {
-switch(action.type){
-    case BUY_CAKE: return {
-        ...state,
-        numCake : state.numCake-1
+// const reducer = (state=intial,action)=> {
+// switch(action.type){
+//     case BUY_CAKE: return {
+//         ...state,
+//         numCake : state.numCake-1
+//     }
+//     case BUY_Icecream: return {
+//         ...state,
+//         numIcecream : state.numIcecream-1
+//     }
+//     default: return state
+// }
+// }
+
+const cakeReducer = (state=intialnumCake,action)=> {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numCake : state.numCake-1
+        }
+        default: return state
     }
-    case BUY_Icecream: return {
-        ...state,
-        numIcecream : state.numIcecream-1
     }
-    default: return state
-}
-}
 
 
+    const iceReducer = (state=intialnumIcecream,action)=> {
+        switch(action.type){
+            case BUY_Icecream: return {
+                ...state,
+                numIcecream : state.numIcecream-1
+            }
+            default: return state
+        }
+        }
+
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    ice:iceReducer
+})
 // store
-const store  = createStore(reducer);
+const store  = createStore(rootReducer);
 console.log('intial state',store.getState());
 const unSubscribe = store.subscribe(()=>console.log('Updated state',store.getState()));
 store.dispatch(buyCake());
