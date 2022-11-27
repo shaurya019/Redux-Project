2c31d81cfe9feffb75a16b61f982674436ebdaa1
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2022-11-27 21:06:36 +0530

    B

--- redux-demo/src/redux/store.js
+++ redux-demo/src/redux/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
 const store = createStore(rootReducer,composeWithDevTools(
-    applyMiddleware(logger),
+    applyMiddleware(logger,thunk)
   ));
 
 export default store;
